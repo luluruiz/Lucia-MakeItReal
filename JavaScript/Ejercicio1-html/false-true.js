ceb1fa6997664e1b39a01652ca03297d996e24a7
LuluRuiz <lulu.ruiz.vega@gmail.com>
2023-06-09 20:08:22 -0500

    subir ejercicios de operadores

---
+++ JavaScript/Ejercicio1-html/false-true.js
@@ -0,0 +1,12 @@
+console.log(1 === 1);    // true
+console.log(1 === "1");  // false
+console.log(3 < 2);      // false
+console.log(2 > 0);      // true
+console.log(7 >= 7);     // true
+console.log(6 <= 5);     // false
+console.log(3 !== 3);    // false
+console.log("hola" !== "hola");  // false
+console.log(4 !== "4");          // true
+console.log(false === true);     // false
+console.log(true);      // true
+console.log(false); // false
---
+++ JavaScript/Ejercicio1-html/numeros.js
@@ -0,0 +1,15 @@
+//Numeros en clase
+
+console.log (4 + 6);
+console.log (25-4);
+console.log (4*5);
+console.log (10/2);
+/* toma por regla matematica las operaciopne como en 
+matematica es decir
+primeor la multiplicacion - se llama precedencia de operadores*/
+
+// = igual se usa para dar valor a algo asi que para decir que 100 es === 100 
+// diferente !== 
+// con 3 === evalua el tipo de dato y == solo el tipado entonces mejor con 3
+// igual siempre con 3 === y diferente siempre !== 
+
